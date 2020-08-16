e598b8c71e53213098b2f636b92dd8ffc0be0056
Steven <steventheodore98@gmail.com>
2020-08-16 13:27:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+const port = 5000;
+
+// index page
+app.get("/api", (req, res) => {
+  // this is my api url, change with yours
+  let url =
+    "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=782f256cab3f792783b80e2b30e10569&tags=cats&format=json&nojsoncallback=1&per_page=20&page=1&tags=cats";
+
+  req.pipe(request(url)).pipe(res);
+});
+
+// to implement paging and searching
+app.get("/api/:page-:tags", (req, res) => {
+  // this is my api url, change with yours
+  let url =
+    "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=782f256cab3f792783b80e2b30e10569&tags=cats&format=json&nojsoncallback=1&per_page=20&page=" +
+    req.params.page +
+    "&tags=" +
+    req.params.tags;
+  req.pipe(request(url)).pipe(res);
+});
+
+// searching
+app.get("/api/search/:tags", (req, res) => {
+  // this is my api url, change with yours
+  let url =
+    "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=782f256cab3f792783b80e2b30e10569&tags=cats&format=json&nojsoncallback=1&per_page=20&tags=" +
+    req.params.tags;
+  req.pipe(request(url)).pipe(res);
+});
+
+// listening to port 3001
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
